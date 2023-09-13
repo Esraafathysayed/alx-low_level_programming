f10c45a98a7891ca1ad6715a612268d705dba058
Esraafathysayed <esraafathy79295@gmail.com>
2023-09-12 17:54:36 -0700

    end file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,6 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -16,7 +15,7 @@ void print_times_table(int n)
 			{
 				k = j * i;
 if (j == 0)
-				{
+	{
 					_putchar(k + '0');
 				} else if (k < 10 && j != 0)
 				{
